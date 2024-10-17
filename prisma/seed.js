88f1f5400c420c0019690a29e4c609e412b21727
pray3m <prem.gtm9@gmail.com>
2024-10-17 17:10:23 +0545

    update seeders

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 import dotenv from "dotenv";
+import { petProducts, sampleProducts } from "./seeders/pet-products";
 
 dotenv.config();
 
@@ -39,93 +40,6 @@ async function main() {
   }
 
   // Sample Products
-  const sampleProducts = [
-    {
-      name: "Organic Dog Food",
-      description: "Nutritious and organic kibble for a healthy dog diet.",
-      price: 39.99,
-      category: "food",
-      stock: 150,
-      imageUrl: "https://via.placeholder.com/600/92c952",
-    },
-    {
-      name: "Squeaky Chew Toy",
-      description:
-        "Durable squeaky toy to keep your dog entertained for hours.",
-      price: 14.99,
-      category: "toys",
-      stock: 300,
-      imageUrl: "https://via.placeholder.com/150/771796",
-    },
-    {
-      name: "Leather Dog Leash",
-      description: "Premium leather leash for comfortable and secure walks.",
-      price: 24.99,
-      category: "accessories",
-      stock: 80,
-      imageUrl: "https://via.placeholder.com/150/771796",
-    },
-    {
-      name: "Flea & Tick Prevention",
-      description:
-        "Effective solution to protect your dog from fleas and ticks.",
-      price: 19.99,
-      category: "healthcare",
-      stock: 120,
-      imageUrl: "https://via.placeholder.com/600/f66b97",
-    },
-    {
-      name: "Pet Grooming Kit",
-      description: "Complete grooming kit for maintaining your pet's hygiene.",
-      price: 29.99,
-      category: "grooming",
-      stock: 60,
-      imageUrl: "https://via.placeholder.com/600/56a8c2",
-    },
-    {
-      name: "Interactive Ball Launcher",
-      description:
-        "Automatic ball launcher to keep your dog active and engaged.",
-      price: 59.99,
-      category: "toys",
-      stock: 40,
-      imageUrl: "https://via.placeholder.com/600/54176f",
-    },
-    {
-      name: "Dental Chews for Dogs",
-      description:
-        "Helps clean teeth and freshen breath while satisfying chewing instincts.",
-      price: 12.99,
-      category: "healthcare",
-      stock: 250,
-      imageUrl: "https://via.placeholder.com/600/372c93",
-    },
-    {
-      name: "Adjustable Dog Harness",
-      description: "Comfortable and adjustable harness for secure walks.",
-      price: 34.99,
-      category: "accessories",
-      stock: 90,
-      imageUrl: "https://via.placeholder.com/600/66b7d2",
-    },
-    {
-      name: "Premium Cat Food",
-      description: "High-quality cat food enriched with essential nutrients.",
-      price: 44.99,
-      category: "food",
-      stock: 130,
-      imageUrl: "https://via.placeholder.com/600/1fe46f",
-    },
-    {
-      name: "Catnip Toys",
-      description: "Fun and stimulating catnip-filled toys for playful cats.",
-      price: 9.99,
-      category: "toys",
-      stock: 200,
-      imageUrl: "https://via.placeholder.com/600/61a65",
-    },
-  ];
-
   for (const product of sampleProducts) {
     const existingProduct = await prisma.product.findFirst({
       where: { name: product.name },
